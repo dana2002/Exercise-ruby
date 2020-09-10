703f3c4828703c885b23abb8430700eb55cdb389
dana2002 <danacastroman@gmail.com>
2020-09-10 16:04:31 -0300

    remove extra blank lines

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -4,15 +4,14 @@ require 'json'
 require 'httparty'
 
 begin
-  puts "nombre del archivo: "
+  puts "ingrese ruta dal archivo: "
   namefile = gets.chomp
   content = File.read(namefile)
-
   puts "descripcion: "
   description = gets.chomp
   puts "Quieres el gist publico? si/no"
   public = gets.chomp
-
+  
   if public == 'si'
     public = true
   elsif public == 'no'
@@ -22,8 +21,6 @@ begin
   end
 
   uri = URI.parse('https://api.github.com/gists')
-
-
   data = {
             'description' => "#{description}",
             'public' => public,
@@ -33,27 +30,24 @@ begin
               }
             }
           }
-        
 
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
   request = Net::HTTP::Post.new(uri.request_uri)
-  request['Authorization'] = 'token c90f2da8cea3cef00e96e96bc1433d7024479715'
-
+  request['Authorization'] = 'token bb3f0a45790a8b18bf1468c70957c55293e7fe32'
   request.body = data.to_json
-  response = http.request(request)
 
+  response = http.request(request)
   json = response.body
   url = JSON.parse(json)
 
   puts url['url']
-  
+
 rescue Errno::ENOENT => e 
   p "Archivo no encontrado"
   retry
-
 rescue SocketError => exception
   p "Error de conexión"
   retry
